b68982980c7e186f3d697e5b8161e095eee0612e
Olli Karppinen <olli.karppinen@relex.fi>
2017-12-16 11:23:31 +0200

    Styling tweaks

--- client/src/App.js
+++ client/src/App.js
@@ -11,16 +11,18 @@ class App extends Component {
         window.fetch('boards')
               .then(response => response.json())
               .then(json => this.setState({ boards: json }))
-              .catch(error => console.log(error))
+              .catch(error => console.log(error));
     }
 
     handleActivateBoard(board) {
-        console.log(board);
         this.setState({ activeBoard: board })
     }
 
-    handleAdd(title, type, ref_id) {
-        console.log(title, type, ref_id);
+    handleAdd(path, payload) {
+        window.fetch(path, { method: 'POST', body: JSON.stringify(payload), headers: { 'Content-Type': 'application/json' }, })
+              .then(response => response.json())
+              .then(json => console.log(json))
+              .catch(error => console.log(error));
     }
 
     render() {
@@ -28,7 +30,7 @@ class App extends Component {
         return (
             this.state.activeBoard ?
                 <Board board={this.state.activeBoard} onClick={() => this.setState({ activeBoard: null })} onAdd={this.handleAdd.bind(this)} /> :
-                <Boards boards={this.state.boards}  onActivate={this.handleActivateBoard.bind(this)} />
+                <Boards boards={this.state.boards}  onActivate={this.handleActivateBoard.bind(this)} onAdd={this.handleAdd.bind(this)} />
         );
     }
 }
@@ -37,11 +39,13 @@ const Boards = props => {
 
     return (
         <div className="boards">
-            <h1>Boards</h1>
+            <div className="title">Boards</div>
             <div className="board-buttons">
                 {props.boards.map(board =>
                     <BoardButton key={board.id} onActivate={() => props.onActivate(board)} {...board} />)}
-                <CreateBoardButton />
+                <div className="add-board big-button">
+                    <Add onAdd={props.onAdd} payload={{}} target="board" path="boards" />
+                </div>
             </div>
         </div>
     )
@@ -49,31 +53,34 @@ const Boards = props => {
 
 const BoardButton = props => {
     return (
-        <div onClick={props.onActivate} className="board-button">
+        <div onClick={props.onActivate} className="board-button big-button">
             {props.title}
         </div>
     )
 };
 
 const CreateBoardButton = props => {
-    return <div className="board-button create-board-button">Create a new table...</div>
+    return <Add onAdd={props.onAdd} payload={{ column_id: props.id }} target="task" path="tasks" />
 };
 
 const Board = props => {
     return (
         <div className="board">
-            <div className="board-title">{props.board.title}<span onClick={props.onClick}>return</span></div>
-            <Columns columns={props.board.columns} onAdd={props.onAdd} />
+            <div className="title">{props.board.title}<span onClick={props.onClick}>return</span></div>
+            <Columns {...props.board} onAdd={props.onAdd} />
         </div>
     )
 };
 
 const Columns = props => {
-
+    console.log(props)
     return (
+
         <div className="columns">
             {props.columns.map(column => <Column key={column.id} {...column} onAdd={props.onAdd} />)}
-            <div className="column"><div className="column-content add-column-button"><Add target="column" /></div></div>
+            <div className="column"><div className="column-content add-column-button">
+                <Add onAdd={props.onAdd} payload={{ board_id: props.id }} target="column" path="columns" />
+            </div></div>
         </div>
     )
 };
@@ -86,7 +93,7 @@ const Column = props => {
             <div className="column-content">
                 <header className="column-title">{props.title}</header>
                 {tasks}
-                <Add onAdd={props.onAdd} target="task" />
+                <Add onAdd={props.onAdd} payload={{ column_id: props.id }} target="task" path="tasks" />
             </div>
         </div>
     )
@@ -104,19 +111,19 @@ class Add extends Component {
     };
 
     render() {
-        return this.state.active ? <AddForm onAdd={this.props.onAdd} onClose={() => this.setState({ active: false })} />: <AddButton onClick={() => this.setState({ active: true })} />;
+        return this.state.active ?
+            <AddForm onAdd={this.props.onAdd}
+                     onClose={() => this.setState({ active: false })}
+                     target={this.props.target}
+                     path={this.props.path}
+                     payload={this.props.payload}
+            />: <AddButton onClick={() => this.setState({ active: true })} target={this.props.target} />;
     }
 }
 
 const AddButton = props => {
     return <div onClick={props.onClick} className={"add-button"}>{`Add ${props.target}...`}</div>;
 };
-
-// const AddForm = props => {
-//     return (
-//
-//     );
-// };
 
 class AddForm extends Component {
     state = {
@@ -129,11 +136,18 @@ class AddForm extends Component {
         });
     }
 
+    onSubmit() {
+        const payload = {};
+        payload[this.props.target] = { ...this.props.payload, title: this.state.inputValue };
+        this.props.onAdd(this.props.path, payload);
+        this.props.onClose();
+    }
+
     render() {
         return (
             <div className="add-form">
-                <input value={this.state.inputValue} onChange={this.updateInputValue.bind(this)} type="text" name="name" className="add-input" />
-                <span onClick={() => this.props.onAdd(this.state.inputValue)} className="add-form-button add-submit">Add</span>
+                <input autoFocus value={this.state.inputValue} onChange={this.updateInputValue.bind(this)} type="text" name="name" className="add-input" />
+                <span onClick={this.onSubmit.bind(this)} className="add-form-button add-submit">Add</span>
                 <span onClick={this.props.onClose}className="add-form-button add-close">Close</span>
             </div>
         );
